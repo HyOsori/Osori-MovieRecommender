d3a5c4b5289b476b04d75ba1de554dd409d681a6
jhyang12345 <jhyang12345@naver.com>
2017-05-28 21:57:41 +0900

    Added comments

--- scrapedirectory.py
+++ scrapedirectory.py
@@ -4,36 +4,38 @@ import requests, codecs, urllib, re
 import urllib.request
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
+from multithreading import multithreading
+
 
 browseurl = 'http://movie.naver.com/movie/sdb/browsing/bmovie.nhn?'
 baseurl = 'http://movie.naver.com'
 
-banlist = []
+banlist = ['TV시리즈', 'TV영화', '단편영화', '비디오영화', '옴니버스영화', '뮤직비디오', '옴니버스영화', '웹드라마', '인터넷영화', '공연실황', '뮤지컬']
 
 def getparams():
-    years = open('years.txt', 'r')
-    countries = open('countries.txt', 'r')
-    years = years.read().split()
+    years = open('years.txt', 'r')# 연도 목록
+    countries = open('countries.txt', 'r')# 나라 목록
+    years = years.read().split()# 연도별로 나눠서 만든 리스트
     countries = countries.read().split()
     ret = []
-    for year in years:
+    for year in years:#연도별 나라별로 루프 돌림
         for country in countries:
             for page in range(1, 50):
-                ret.append(urllib.parse.urlencode((('open', str(year)), ('nation', str(country)), ('page', str(page)))))
+                ret.append(urllib.parse.urlencode((('open', str(year)), ('nation', str(country)), ('page', str(page)))))#반환할 리스트에 추가
     #print(ret)
     return ret
 
 def getmovies(param, dbh):
-    url = browseurl + param
-    try:
-        with urllib.request.urlopen(url) as response:
-            htmlcode = response.read().decode(response.headers.get_content_charset(), errors='replace')
+    url = browseurl + param#url 합성
+    try:#Exception 대비해서 try문 안에 넣음
+        with urllib.request.urlopen(url) as response:#with 문 안에 열어서 url 열고 닫는 문제 해결
+            htmlcode = response.read().decode(response.headers.get_content_charset(), errors='replace')#한글 해석을 위한 부분
 
-            soup = BeautifulSoup(htmlcode, 'html.parser')
+            soup = BeautifulSoup(htmlcode, 'html.parser')#html 파서 initialize
 
-            movielist = soup.findAll("ul", {"class": "directory_list"})[0].findAll('a', class_=lambda x: x != 'green')
+            movielist = soup.findAll("ul", {"class": "directory_list"})[0].findAll('a', class_=lambda x: x != 'green')#ul 요소에서 class 구별해서 스크레이핑
 
-            movieul = soup.findAll("ul", {"class": "directory_list"})[0]
+            movieul = soup.findAll("ul", {"class": "directory_list"})[0]#리스트 변수에 저장
 
             for li in movieul.children:
                 soup = BeautifulSoup(str(li), 'html.parser')
@@ -47,10 +49,10 @@ def getmovies(param, dbh):
                     movieitem = {'title': title, 'link': link, 'code': code}
                     print("Reviewing movie: " + title)
                     #print(movieitem)
-                    if dbh.movielist.find({'code': code}).count() < 1:
-                        dbh.movielist.insert_one(movieitem)
+                    if dbh.movielist.find({'code': code}).count() < 1:#디비에 없는 경우
+                        dbh.movielist.insert_one(movieitem)#한줄 넣기
                         print("Inserting movie: " + title)
-                    elif dbh.movielist.find({'code': code}).count() == 1:
+                    elif dbh.movielist.find({'code': code}).count() == 1:#디비에 이미 들어가있는 경우
                         #print(soup)
                         seen = False
                         for ban in banlist:
@@ -63,11 +65,11 @@ def getmovies(param, dbh):
 
             for movie in movielist:
                 try:
-                    title = movie.text
+                    title = movie.text#요소 분석
                     link = baseurl + movie['href']
                     code = link[link.index('=') + 1:]
-                    movieitem = {'title': title, 'link': link, 'code': code}
-                    if dbh.movielist.find({'code': code}).count() < 1:
+                    movieitem = {'title': title, 'link': link, 'code': code}#아이템 생성 후 넣기
+                    if dbh.movielist.find({'code': code}).count() < 1:#디비에 없는 경우
                         dbh.movielist.insert_one(movieitem)
                         print("Inserting movie: " + title)
 
@@ -81,15 +83,16 @@ def getmovies(param, dbh):
 
 def main():
     try:
-        c = MongoClient(host="localhost", port=27018)
-        dbh = c['moviedb']
+        c = MongoClient(host="localhost", port=27018)#디비에 연결 포트는 다른 작업을 위해 27018에 함 db 열어줄때 따로 써줘야한다
+        dbh = c['moviedb']#생성할/된 db 이름
     except:
         print("Error connecting to database!")
         return
 
     params = getparams()
-    for param in params:
-        getmovies(param, dbh)
+
+    multithreading(getmovies, [[browseurl + param, dbh] for param in params])
+    #멀티 스레딩 함수를 통해 동시에 10개씩 크롤링
 
 if __name__ == '__main__':
     main()
