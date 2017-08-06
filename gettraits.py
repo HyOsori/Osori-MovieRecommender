43aefaf0e60dd1dde5365dea77f00b1233394638
jhyang12345 <jhyang12345@naver.com>
2017-08-06 23:00:03 +0900

    get main actors and director

--- gettraits.py
+++ gettraits.py
@@ -8,6 +8,34 @@ from multithreading import multithreading
 
 baseurl = 'http://movie.naver.com'
 
+def getmainactors(movie, dbh):
+    baseurl = 'http://movie.naver.com/movie/bi/mi/detail.nhn?code='
+    try:
+        print("Retrieving main actors.")
+        ret = []
+        with urllib.request.urlopen(baseurl + movie['code']) as page:
+            page = page.read().decode(page.headers.get_content_charset(), errors='replace')
+            soup = BeautifulSoup(page, 'html.parser')
+            actors = soup.findAll('div', {'class': 'p_info'})
+            for actor in actors:
+                if '주연' in str(actor):
+                    actoritem = {}
+                    name = actor.find('a').text
+                    link = baseurl + actor.find('a')['href']
+                    baselink = actor.find('a')['href']
+                    code = baselink[baselink.index('=') + 1:]
+                    actoritem = {'name': name, 'link': link, 'code': code}
+                    print("Actor: " + name + " " + code)
+                    ret.append(actoritem)
+                    if dbh.actorlist.find({'code': code}).count() < 1:
+                        dbh.actorlist.insert_one(actoritem)
+                        print("Inserting:", name)
+            return ret
+    except Exception as inst:
+        print("Invalid movie!")
+        print(inst)
+        return
+
 def gettraits(movie, dbh):
 
     moviecode = movie["code"]
@@ -64,6 +92,44 @@ def gettraits(movie, dbh):
             validity = True
             if(finalraters < 75):
                 validity = False
+
+            director = ''
+            infospec = soup.find("dl", {"class": "info_spec"})
+
+            newsoup = BeautifulSoup(str(infospec), 'html.parser')
+
+            director = newsoup.find('dt', {'class': 'step2'})
+
+            #retrieve the director
+            if director != None:
+                holder = director.find_next_siblings()[0]
+                if holder.find('a') != None:
+                    directorname = holder.find('a').text
+                    directorlink = holder.find('a')['href']
+                    directorcode = directorlink[directorlink.index('=') + 1:]
+                    directoritem = {'name': directorname, 'link': directorlink, 'code': directorcode}
+                    if dbh.directorlist.find({'code': directorcode}).count() < 1:
+                        dbh.directorlist.insert_one(directoritem)
+                    dbh.movielist.update({'_id': movie['_id']}, {'$set': {'director': directoritem}})
+                    print(directorname, directorcode)
+                else:
+                    dbh.movielist.update({'_id': movie['_id']}, {'$set': {"valid": False}})
+                    print("Invalid movie!")
+                    print("Not found!")
+                    return
+            else:
+                dbh.movielist.update({'_id': movie['_id']}, {'$set': {"valid": False}})
+                print("Invalid movie!")
+                print("Not found!")
+                return
+            mainactors = getmainactors(movie, dbh)
+            if mainactors:
+                dbh.movielist.update({'_id': movie['_id']}, {'$set': {'actors': getmainactors(movie, dbh)}})
+            else:
+                dbh.movielist.update({'_id': movie['_id']}, {'$set': {'valid': False}})
+                print("Invalid movie!")
+                return
+
             print("Scraping movie: " + movie["title"] + " " + \
                 str(finalrating) + " " + str(validity))
 
