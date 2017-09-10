64d4f73ec4265525df41ffec0b1ab1bd6938bbe1
jhyang12345 <jhyang12345@naver.com>
2017-09-10 23:24:47 +0900

    added choosegenre to recommendations

--- recommendations.py
+++ recommendations.py
@@ -1,5 +1,6 @@
 import sys
 from pymongo import MongoClient
+from getdistance import totaldistance
 
 genrelist = ['블랙코미디', 'SF', '애니메이션', '느와르', '서스펜스', '모험', '멜로/로맨스', '서사', '코미디', '다큐멘터리', '서부', '무협', '전쟁', '드라마', '스릴러', '에로', '가족', '액션', '공연실황', '공포', '범죄', '판타지', '미스터리', '실험', '뮤지컬']
 
@@ -14,7 +15,7 @@ def genretogenre(dbh):
                     counter[item] = 0
             counter['type'] = genre
             dbh.genrecounts.insert_one(counter)
-            print("Inserting:", counter)
+            #print("Inserting:", counter)
 
     for movie in list(dbh.movielist.find({'valid': True})):
         if('genre' not in movie):
@@ -23,8 +24,51 @@ def genretogenre(dbh):
             for y in movie['genre']:
                 if x != y:
                     dbh.genrecounts.update_one({'type': x}, {'$inc': {y: 1}})
-                    print("Incrementing", x, "for", y)
+                    #print("Incrementing", x, "for", y)
 
+def choosegenre(genre, dbh, seen, cands=10):
+    thisgenre = dbh.genrecounts.find_one({'type': genre})
+    counter = {}
+    total = 0
+
+    originalgenre = genre#original genre that is being referenced
+    for genre in thisgenre:
+        if genre != '_id' and genre != 'type':
+            counter[genre] = thisgenre[genre]
+            total += counter[genre]
+    counter[originalgenre] = total * 2 // 3
+    total = total + counter[genre]
+    keys = list(counter.keys())
+    #print(keys)
+    ranges = [0]
+    for i in range(len(keys)):
+        ranges.append(ranges[i] + counter[keys[i]])
+        #print(counter[key])
+
+    genrecands = {}
+    for key in counter:
+        if counter[key] != 0:
+            genrecands[key] = []
+            #include the original genre among the candidates
+            movielist = list(dbh.movielist.find({'valid': True, 'genre': {'$in': [key, originalgenre]}}).sort('rating', -1))
+            while(len(genrecands[key]) < cands and movielist != []):
+                movie = movielist.pop(0)
+                if movie['code'] not in seen:
+                    genrecands[key].append(movie)
+            random.shuffle(genrecands[key])
+    ret = []
+    genrespicked = []
+    for _ in range(cands):
+        val = random.random() * total
+        found = 0
+        for i in range(len(keys)):
+            if ranges[i] < val < ranges[i + 1] and counter[keys[i]] != 0:
+                found = i
+                ret.append(genrecands[keys[i]].pop())
+                genrespicked.append(keys[i])
+                break
+    print(genrespicked)
+    return ret
 
 def main():
     try:
@@ -34,6 +78,9 @@ def main():
         print("Unable to connect!")
         sys.exit(1)
     genretogenre(dbh)
+    with open('movieseen.txt', 'r') as movieseen:
+        movieseen = movieseen.read().split()
+
 
 if __name__ == '__main__':
     main()
